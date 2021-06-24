a1f95a2ba466205c86518585ce4bf33a10c41fcc
Cristian Saldivia <cristiansaldivia@MacBook-Pro-de-Cristian.local>
2021-06-24 13:07:01 -0300

    modificando numberRandom function

--- src/utils/numberRandom.util.js
+++ src/utils/numberRandom.util.js
@@ -1,4 +1,24 @@
-const ARRAY_OF_NUMBER = Array.from({ length: 25 }, (v, i) => i);
+const MAX = 100;
+const MIN = 1;
+const ARRAY_LENGTH = 20;
+
+const generateNumberRandom = () => {
+  return Math.floor(Math.random() * (MAX + 1 - MIN) + MIN);
+};
+
+const generateArrayNumberRandom = () => {
+  const arraySet = new Set();
+  let number;
+  while (arraySet.size < ARRAY_LENGTH) {
+    number = generateNumberRandom();
+    arraySet.add(number);
+  }
+  return [...arraySet];
+};
+
+const ARRAY_OF_NUMBER = generateArrayNumberRandom();
+
 export const numberRandom = () => {
   return ARRAY_OF_NUMBER.pop();
 };
+
