f9ada6762276b65c07aa4a95a46789ed0f5ed1cd
Franco Abosso <franco.abosso@gm2dev.com>
2021-07-16 10:25:11 -0300

    Add legibilize function

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -5,3 +5,16 @@ export function normalizeId(id) {
 export function normalizeCard(number) {
   return (number / 10).toLocaleString("es-AR");
 }
+
+export function legibilize(text) {
+  const capitalizableTexts = ["hp"];
+  const legibleWords = [];
+  text.split("-").forEach((word) => {
+    if (capitalizableTexts.includes(word)) {
+      legibleWords.push(word.toUpperCase());
+    } else {
+      legibleWords.push(word.charAt(0).toUpperCase() + word.slice(1));
+    }
+  });
+  return legibleWords.join(" ");
+}
